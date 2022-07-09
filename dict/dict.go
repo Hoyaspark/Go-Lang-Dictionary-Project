9e233f2abaf0864df8dbca9dfb06661554a73e90
박상호 <hoyaspark@gmail.com>
2022-07-09 20:16:43 +0900

    feat : make Search Method

--- dict/dict.go
+++ dict/dict.go
@@ -1,4 +1,22 @@
 package dict
 
+import "errors"
+
 // Dictionary type
 type Dictionary map[string]string
+
+var errNotFound error = errors.New("not Found")
+
+func (d Dictionary) Search(word string) (string, error) {
+	value, exists := d[word]
+
+	if exists {
+		return value, nil
+	}
+
+	return "", errNotFound
+}
+
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
+}
--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"dictionaryProject/dict"
 	"fmt"
+	"log"
 )
 
 func main() {
-	dictionary := dict.Dictionary{}
-	fmt.Println(dictionary)
+	dictionary := dict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("second")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(definition)
+
 }
